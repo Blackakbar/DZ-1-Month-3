e136994ad251db17027e12ca576e262fa4e5450d
Blackakbar <akbar04222@gmail.com>
2023-09-12 17:03:21 +0600

    commit1

--- js/home_works.js
+++ js/home_works.js
@@ -17,22 +17,61 @@ gmailButton.onclick = () => {
 }
 
 
-// BLOCK MOVE
-const childBlock = document.querySelector('.child_block');
+// MOVE BLOCK
+const childBlock = document.querySelector('.child_block')
 
-const moveRight = (childBlock) => {
-    let currentLeft = 0;
+let positionX = 0
+let positionY = 0
 
-    const move = () => {
-        if (currentLeft < 448) {
-            currentLeft++
-            childBlock.style.left = currentLeft + 'px'
-
-            requestAnimationFrame(move);
-        }
+const move = () => {
+    if (positionX < 448 && positionY === 0) {
+        positionX++
+        childBlock.style.left = `${positionX}px`
+    } else if (positionX === 448 && positionY < 448) {
+        positionY++
+        childBlock.style.top = `${positionY}px`
+    } else if (positionX > 0 && positionY === 448) {
+        positionX--
+        childBlock.style.left = `${positionX}px`
+    } else if (positionX === 0 && positionY > 0) {
+        positionY--
+        childBlock.style.top = `${positionY}px`
     }
-
-    move()
+    setTimeout(move, 1)
 }
 
-moveRight(childBlock)
+move()
+
+// STOP WATCH
+const startButton = document.querySelector('#start')
+const stopButton = document.querySelector('#stop')
+const resetButton = document.querySelector('#reset')
+const interval = document.querySelector('#secondsS')
+
+let intervalTime
+let time = 0
+
+function updateTimer() {
+    interval.textContent = time
+}
+
+startButton.addEventListener('click', () => {
+    if (!intervalTime) {
+        intervalTime = setInterval(() => {
+            time++
+            updateTimer();
+        }, 1000)
+    }
+})
+
+stopButton.addEventListener('click', () => {
+    clearInterval(intervalTime)
+    intervalTime = null
+})
+
+resetButton.addEventListener('click', () => {
+    clearInterval(intervalTime)
+    intervalTime = null
+    time = 0
+    updateTimer()
+})
